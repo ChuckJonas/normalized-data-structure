ab9fb625b6ec0ab49bcf714a9865bc01db8c870f
Charlie Jonas <charlie@callawaycloudconsulting.com>
2020-02-20 17:27:18 -0700

    - added toArray - Fixed types - add readme

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Normalized Data Structure
+
+A very simple library to make it easier to work with [Redux Normalizing State Shape](https://redux.js.org/recipes/structuring-reducers/normalizing-state-shape/), in an immutable way.
+
+Written in typescript.
+
+## Usage
+
+- `empty<T>`: create a new normalized data structure of type `T` `{ byId: {}, allIds: [] }`
+- `forEach`: loop through each item
+- `map`: map items returning a new Array
+- `filter`: filter items, returning a new Array
+- `set`: add an item by key, over-writing any previous item
+- `addItems`: add a array of items to the structure.  Takes either a `keyof T` or a selector function that returns a string to specify the structure `key`.
+- `removeItem`: Remove item by key
+- `fromArray`: creates a new "normalized" object from an array.  Takes either a `keyof T` or a selector function that returns a string to specify the structure `key`.
+- `toArray`: get all items as an array
+
--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,101 @@
 export interface Normalized<T> {
-    byId: { [id: string]: T };
-    allIds: string[];
+  byId: { [id: string]: T };
+  allIds: string[];
 }
 
-export const normalized = {
-    empty: <T>() => {
-      const obj: Normalized<T> = {
-        byId: {},
-        allIds: [],
-      };
-      return obj;
-    },
-    forEach: <T>(normalizedObjs: Normalized<T>, callback: (obj: T, key: string) => void) => {
-      normalizedObjs.allIds.forEach((id) => {
-        callback(normalizedObjs.byId[id], id);
-      });
-    },
-    map: <T>(normalizedObjs: Normalized<T>, callback: (obj: T) => any) => {
-      const ret: any[] = [];
-      normalizedObjs.allIds.forEach((id) => {
-        ret.push(callback(normalizedObjs.byId[id]));
-      });
-      return ret;
-    },
-    filter: <T>(normalizedObjs: Normalized<T>, callback: (obj: T) => boolean): T[] => {
-      const ret: T[] = [];
-      normalizedObjs.allIds.forEach((id) => {
-        const item = normalizedObjs.byId[id];
-        if (callback(item)) {
-          ret.push(item);
-        }
-      });
-      return ret;
-    },
-    set: <T>(normalizedObjs: Normalized<T>, key: string, item: T): Normalized<T> => {
-      const byId = { ...normalizedObjs.byId, [key]: item };
-      let allIds = normalizedObjs.allIds;
-      if (allIds.indexOf(key) === -1) {
-        allIds = normalizedObjs.allIds.concat(key);
-      }
-      return { byId, allIds };
-    },
-    addItems: <T, K extends keyof T>(normalizedObjs: Normalized<T>, keySelector: (item:T) => string, items: T[]): Normalized<T> => {
-      const newByIds = normalized.empty<T>().byId;
-      const allIds = normalizedObjs.allIds.slice();
-      for (const item of items){
-        const index = keySelector(item);
-        newByIds[index] = item;
-        if (allIds.indexOf(index) === -1) {
-          allIds.push(index);
-        }
-      }
-      return {
-        byId: {...normalizedObjs.byId, ...newByIds},
-        allIds,
-      };
-    },
-    removeItem: <T>(normalizedObjs: Normalized<T>, key: string): Normalized<T> => {
-      const byId = { ...normalizedObjs.byId };
-      delete byId[key];
+type KeySelector<T> = (item: T) => string;
 
-      const allIds = normalizedObjs.allIds.filter((id) => {
-        return id !== key;
-      });
-      return { byId, allIds };
-    },
-    fromArray: <T>(arr: T[], idFunction: (obj: T) => string): Normalized<T> => {
-      const allIds: string[] = [];
-      const byId = normalized.empty<T>().byId;
-      arr.forEach((item) => {
-        const id = idFunction(item);
-        allIds.push(id);
-        byId[id] = item;
-      });
-      return {
-        allIds,
-        byId,
-      };
-    },
-  };
+export const normalized = {
+  /**
+   * Creates a new Empty normalized object
+  */
+  empty: <T>() => {
+    const obj: Normalized<T> = {
+      byId: {},
+      allIds: [],
+    };
+    return obj;
+  },
+
+  forEach: <T>(normalizedObjs: Normalized<T>, callback: (obj: T, key?: string, i?: number) => void) => {
+    normalizedObjs.allIds.forEach((id, i) => {
+      callback(normalizedObjs.byId[id], id, i);
+    });
+  },
+  map: <T>(normalizedObjs: Normalized<T>, callback: (obj: T) => any) => {
+    const ret: any[] = [];
+    normalizedObjs.allIds.forEach((id) => {
+      ret.push(callback(normalizedObjs.byId[id]));
+    });
+    return ret;
+  },
+  filter: <T>(normalizedObjs: Normalized<T>, callback: (obj: T) => boolean): T[] => {
+    const ret: T[] = [];
+    normalizedObjs.allIds.forEach((id) => {
+      const item = normalizedObjs.byId[id];
+      if (callback(item)) {
+        ret.push(item);
+      }
+    });
+    return ret;
+  },
+  set: <T>(normalizedObjs: Normalized<T>, key: string, item: T): Normalized<T> => {
+    const byId = { ...normalizedObjs.byId, [key]: item };
+    let allIds = normalizedObjs.allIds;
+    if (allIds.indexOf(key as string) === -1) {
+      allIds = normalizedObjs.allIds.concat(key);
+    }
+    return { byId, allIds };
+  },
+  addItems: <T>(normalizedObjs: Normalized<T>, keySelector: keyof T | KeySelector<T>, items: T[]): Normalized<T> => {
+    const newByIds = normalized.empty<T>().byId;
+    const allIds = normalizedObjs.allIds.slice();
+    for (const item of items) {
+      let key = getKey(item, keySelector);
+
+      newByIds[key] = item;
+      if (allIds.indexOf(key) === -1) {
+        allIds.push(key);
+      }
+    }
+    return {
+      byId: { ...normalizedObjs.byId, ...newByIds },
+      allIds,
+    };
+  },
+  removeItem: <T>(normalizedObjs: Normalized<T>, key: string): Normalized<T> => {
+    const byId = { ...normalizedObjs.byId };
+    delete byId[key];
+
+    const allIds = normalizedObjs.allIds.filter((id) => {
+      return id !== key;
+    });
+    return { byId, allIds };
+  },
+  fromArray: <T>(arr: T[], keySelector: keyof T | KeySelector<T>): Normalized<T> => {
+    const allIds: string[] = [];
+    const byId = normalized.empty<T>().byId;
+    arr.forEach((item) => {
+      let key = getKey(item, keySelector);
+      allIds.push(key);
+      byId[key] = item;
+    });
+    return {
+      allIds,
+      byId,
+    };
+  },
+  toArray: <T>(normalizedObjs: Normalized<T>): T[] => {
+    return normalizedObjs.allIds.map(id => normalizedObjs.byId[id]);
+  }
+};
+
+function getKey<T>(item: T, keySelector: keyof T | KeySelector<T>): string {
+  let index: string;
+  if (keySelector instanceof Function){
+    index = keySelector(item);
+  }else{
+    index = item[keySelector] as any as string;
+  }
+  return index;
+}
